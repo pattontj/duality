536234684995e656c349cebd78b1d2d6b4f9dd76
pattontj <60120527+pattontj@users.noreply.github.com>
2022-01-30 12:37:10 -0800

    scale rotational movement speed by distance

--- player.gd
+++ player.gd
@@ -3,14 +3,16 @@ extends Area2D
 
 export var rotate_point = Vector2(0,0)
 export var rotate_angle: float
-export var rotation_speed = 2.0
+export var rotation_speed = 120.0
+export var distance_speed = 2.0
 var dist_from_center = 100
 
 
 onready var _animated_sprite = $AnimatedSprite
 var velocity = Vector2()
  
-var direction
+var vert_direction
+
 var hitting = 0
 
 #get_viewport().size.x
@@ -29,36 +31,39 @@ func _physics_process(delta):
 	
 	var rot = self.rotation
 	var rotation_speed_local = rotation_speed
+	var distance_speed_local = distance_speed
+	
 	if self.position.angle() >= 0:
 		_animated_sprite.play("dark")
 		if no_angular_movement():
-			direction = 1
+			vert_direction = 1
 		
 		#print("normal")
 	
 	if self.position.angle() < 0:
 		_animated_sprite.play("light")
 		if no_angular_movement():
-			direction = -1
+			vert_direction = -1
 		#print("inverted")
 
 	if Input.is_key_pressed(KEY_SHIFT):
 		rotation_speed_local = rotation_speed/2
+		distance_speed_local = distance_speed/2
 	# TODO: input kinda janky
 	if Input.is_key_pressed(KEY_LEFT):
-		self.position = self.position.rotated(direction * rotation_speed_local * delta)
+		self.position = self.position.rotated(1 * rotation_speed_local * delta * (1/dist_from_center))
 		
 	if Input.is_key_pressed(KEY_RIGHT):
-		self.position = self.position.rotated(-direction * rotation_speed_local * delta)
+		self.position = self.position.rotated(-1 * rotation_speed_local * delta * (1/dist_from_center))
 		
 	if Input.is_key_pressed(KEY_UP):
-		self.position = self.position + self.position.normalized()*rotation_speed_local
+		self.position = self.position + self.position.normalized()*distance_speed_local
 		
 	if Input.is_key_pressed(KEY_DOWN):
-		self.position = self.position+ self.position.normalized()*-rotation_speed_local
-
-
+		self.position = self.position+ self.position.normalized()*-distance_speed_local
 	
+	
+	dist_from_center = sqrt( pow(self.position.x,  2 ) + pow(self.position.y, 2 ) )
 	
 	
 	
