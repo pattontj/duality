21258e6325e8c23a2c48e5994af3e97483d77fd0
pattontj <60120527+pattontj@users.noreply.github.com>
2022-03-10 22:32:10 -0800

    started work on the bullet rework

---
+++ entities/bullets/GenericBullet.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+class GenericBullet:
+	var position
+	var speed = 1.0
+	# The body is stored as a RID, which is an "opaque" way to access resources.
+	# With large amounts of objects (thousands or more), it can be significantly
+	# faster to use RIDs compared to a high-level approach.
+	var body = RID()
+	var angle = 0.0
+	var timer: Timer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
